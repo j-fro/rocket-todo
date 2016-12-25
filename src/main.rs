d56f54f8d67d28a6325254a5a362e0a3115ad076
Jacob Froman <jacob.h.froman@gmail.com>
2016-12-24 22:02:54 -0800

    added put route and more template info

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,17 @@ extern crate serde_derive;
 
 use rocket::request::Form;
 use rocket::response::Redirect;
-use rocket_contrib::Template;
+use rocket_contrib::{Template, JSON};
 use postgres::{Connection, TlsMode};
 
-#[derive(Serialize)]
-#[derive(Debug)]
-#[derive(FromForm)]
+#[derive(Serialize, Deserialize, Debug, FromForm)]
 struct Task {
+    id: i32,
     name: String,
     complete: bool
 }
+
+const CONN_STRING: &'static str = "postgres://jacobfroman@localhost:5432/rust-todo";
 
 #[get("/")]
 fn index() -> Template {
@@ -36,11 +37,19 @@ fn new_task(task: Form<Task>) -> Redirect {
     Redirect::to("/")
 }
 
+#[put("/", format="application/json", data="<task>")]
+fn edit_task(task: JSON<Task>) -> Redirect {
+    println!("{:?}", task);
+    update_task(&task);
+    Redirect::to("/")
+}
+
 fn query_tasks() -> Vec<Task> {
-    let conn = Connection::connect("postgres://jacobfroman@localhost:5432/rust-todo", TlsMode::None).unwrap();
+    let conn = Connection::connect(CONN_STRING, TlsMode::None).unwrap();
     let mut tasks = Vec::new();
     for row in &conn.query("SELECT * FROM tasks", &[]).unwrap() {
         let task = Task {
+            id: row.get(0),
             name: row.get(1),
             complete: row.get(2)
         };
@@ -50,10 +59,18 @@ fn query_tasks() -> Vec<Task> {
 }
 
 fn insert_task(task: &Task) -> () {
-    let conn = Connection::connect("postgres://jacobfroman@localhost:5432/rust-todo", TlsMode::None).unwrap();
+    let conn = Connection::connect(CONN_STRING, TlsMode::None).unwrap();
     conn.execute("INSERT INTO tasks (name, complete) VALUES ($1, $2)", &[&task.name, &task.complete]).unwrap();
 }
 
+fn update_task(task: &Task) -> () {
+    let conn = Connection::connect(CONN_STRING, TlsMode::None).unwrap();
+    conn.execute("UPDATE tasks SET name=$1, complete=$2 WHERE id=$3",
+        &[&task.name, &task.complete, &task.id]).unwrap();
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index, new_task]).launch()
+    rocket::ignite()
+        .mount("/", routes![index, new_task, edit_task])
+        .launch()
 }
